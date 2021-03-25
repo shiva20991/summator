684cc7b5f42ef24224d3597570de9f461063ef56
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-03-25 16:33:00 +0530

    Update wrong_room_handler.py

--- bot/handlers/wrong_room_handler.py
+++ bot/handlers/wrong_room_handler.py
@@ -1,7 +1,7 @@
 from pyrogram import Client
 from bot import LOCAL, CONFIG
 
-async def func(client : Client, message: Message):
+async def func(client : Client, message):
     if message.chat.type == "private":
         try:
             await message.delete()
