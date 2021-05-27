1efb7370fd4389e82e031e15f38d26f1705e7dfd
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-05-27 20:20:23 +0530

    update leech_list_handler.py

--- bot/handlers/leech_list_handler.py
+++ bot/handlers/leech_list_handler.py
@@ -11,6 +11,7 @@ LOGGER = logging.getLogger(__name__)
 from os.path import join as os_path_join
 from pyrogram import Client, filters
 from aria2p import Download
+from pyrogram.types import Message 
 from bot import LOCAL, STATUS, CONFIG, COMMAND
 from bot.plugins import aria2
 
