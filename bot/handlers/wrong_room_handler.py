698297150459ce5643d258ef0c2f3aeffeee5572
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-05-27 16:25:41 +0530

    update wrong_room_handler.py

--- bot/handlers/wrong_room_handler.py
+++ bot/handlers/wrong_room_handler.py
@@ -7,9 +7,3 @@ async def func(client : Client, message):
             await message.delete()
         except:
             pass
-    else:
-        await message.reply_text(
-            LOCAL.WRONG_ROOM.format(
-                CHAT_ID = message.from_user.id            
-            )
-        )
