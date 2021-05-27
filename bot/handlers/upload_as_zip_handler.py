f6dfcd254ae59cb9e857318226938b5cd0b99452
Zack-Bloodshot <77882584+Zack-Bloodshot@users.noreply.github.com>
2021-05-27 20:24:54 +0530

    update upload_as_zip_handler.py

--- bot/handlers/upload_as_zip_handler.py
+++ bot/handlers/upload_as_zip_handler.py
@@ -1,5 +1,6 @@
 from pyrogram import Client, filters
 from bot import LOCAL, STATUS, COMMAND
+from pyrogram.types import Message 
 
 @Client.on_message(filters.command(COMMAND.UPLOAD_AS_ZIP))
 async def func(client : Client):
